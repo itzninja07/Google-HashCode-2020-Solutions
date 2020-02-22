e910add0434ef392de84cd82e02c13bacdd7f48d
Vishal Sharma <www.vishalaharma007@gmail.com>
2020-02-23 01:22:23 +0530

    Add files via upload

---
+++ Basic inputs/main.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int a=2;
+int b=0;
+int c=1;
+int d=3;
+int e=4;
+int f=5;
+cout<<a<<endl;
+cout<<b<<" "<<f<<endl;
+cout<<b<<" "<<c<<" "<<a<<" "<<d<<" "<<e<<endl;
+cout<<c<<" "<<a<<endl;
+cout<<f<<" "<<a<<endl;
+return 0;
+
+}
